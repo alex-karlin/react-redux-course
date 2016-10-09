1141d7160d1bbb28e429b99b31c6bcb0d2600a82
Alex Karlin <kiwi.oleski@gmail.com>
2016-10-09 19:42:22 +1300

    Implement initial application structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.scss'; // Webpack can import CSS files too
+// require('./styles/styles.scss');
+
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
